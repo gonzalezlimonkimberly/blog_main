d2b480eeb179a4c65416a0fb437e0dbf6f0b119e
gonzalezlimonkimberly <gonzalezlimonkimberly1@gmail.com>
2026-03-23 17:48:00 -0700

    actualice views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404, render
+
+from .models import Post, Category
+
 
 # Create your views here.
+def home(request):
+    posts = Post.objects.filter(Status=Post.ACTIVE).order_by('-created_at')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
