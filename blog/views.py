3e77d5343ef068662893dd2a5d3e17c6695dfd06
gonzalezlimonkimberly <gonzalezlimonkimberly1@gmail.com>
2026-04-16 17:33:39 -0700

    Actualice el forms,views y detail

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,8 @@
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 
-
 from .models import Post, Category
 from .forms import CommentForm
-
 
 # Create your views here.
 def home(request):
@@ -15,24 +13,23 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
-
 def detail(request, id):
     post = get_object_or_404(Post, id=id, status=Post.ACTIVE)
-   
+
     if request.method == 'POST':    
         form = CommentForm(request.POST)
-
 
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
 
-
             return redirect('post_detail')
-
+    else:
+        form=CommentForm()    
 
     context = {
         'post': post,
+        'form': form
     }  
     return render (request, 'blog/detail.html',context)
